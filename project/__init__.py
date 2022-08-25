4f0fa0ae412c6fd033ac6a8fcd46078727a362f7
AbeYuki <s121rmorris@gmail.com>
2022-08-25 23:24:30 +0900

    Modify create_app

--- project/__init__.py
+++ project/__init__.py
@@ -3,14 +3,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
+import string
+import secrets
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    alphabet = string.ascii_letters + string.digits
+    secret = ''.join(secrets.choice(alphabet) for i in range(24))
+    app.config['SECRET_KEY'] = secret
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
